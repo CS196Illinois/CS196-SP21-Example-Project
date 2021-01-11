7fd590505eff85fbde258a6230c84159bbc8df27
Matt Geimer <mgeimer2@illinois.edu>
2021-01-10 20:13:16 -0600

    feat: MergeSort and MergeSortHelper

--- Project/Sources/MergeSort/MergeSort.swift
+++ Project/Sources/MergeSort/MergeSort.swift
@@ -1,11 +1,21 @@
 import Foundation
 
 public func MergeSort<T: Comparable>(_ arr: [T]) -> [T] {
-    return arr
+    return MergeSortHelper(arr)
 }
 
 func MergeSortHelper<T: Comparable>(_ arr: [T]) -> [T] {
-    return arr
+    // If array is only one item or empty, return it
+    if arr.count <= 1 {
+        return arr
+    }
+    
+    // Make recursive call sorting each half of the array
+    let left = MergeSortHelper(Array(arr[0 ..< arr.count / 2]))
+    let right = MergeSortHelper(Array(arr[arr.count / 2 ..< arr.count]))
+    
+    // Merge the two sorted sub-arrays and return the resulting array
+    return Merge(left, right)
 }
 
 func Merge<T: Comparable>(_ left: [T], _ right: [T]) -> [T] {
