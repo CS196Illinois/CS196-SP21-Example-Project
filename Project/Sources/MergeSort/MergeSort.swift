0e1b120ccc2074a53c4225db2995b8ba69cb6b04
Matt Geimer <mgeimer2@illinois.edu>
2021-01-10 19:48:13 -0600

    feat: Add function headers for helper functions

--- Project/Sources/MergeSort/MergeSort.swift
+++ Project/Sources/MergeSort/MergeSort.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-func MergeSort<T: Comparable>(_ arr: [T]) -> [T] {
+public func MergeSort<T: Comparable>(_ arr: [T]) -> [T] {
     return arr
+}
+
+private func MergeSortHelper<T: Comparable>(_ arr: [T]) -> [T] {
+    return arr
+}
+
+private func Merge<T: Comparable>(_ left: [T], _ right: [T]) -> [T] {
+    return left
 }
