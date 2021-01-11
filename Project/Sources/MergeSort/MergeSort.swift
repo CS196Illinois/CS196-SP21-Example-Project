f42e5a803b0acb4acced20d612bdc2f2a9492fdc
Matt Geimer <mgeimer2@illinois.edu>
2021-01-10 20:08:39 -0600

    feat: Finish merge function

--- Project/Sources/MergeSort/MergeSort.swift
+++ Project/Sources/MergeSort/MergeSort.swift
@@ -4,10 +4,36 @@ public func MergeSort<T: Comparable>(_ arr: [T]) -> [T] {
     return arr
 }
 
-private func MergeSortHelper<T: Comparable>(_ arr: [T]) -> [T] {
+func MergeSortHelper<T: Comparable>(_ arr: [T]) -> [T] {
     return arr
 }
 
-private func Merge<T: Comparable>(_ left: [T], _ right: [T]) -> [T] {
-    return left
+func Merge<T: Comparable>(_ left: [T], _ right: [T]) -> [T] {
+    var toReturn: [T] = []
+    
+    var leftIndex = 0
+    var rightIndex = 0
+    
+    while leftIndex < left.count || rightIndex < right.count {
+        //Handle edge cases where one array is shorter than the other
+        if leftIndex >= left.count {
+            toReturn.append(right[rightIndex])
+            rightIndex += 1
+            continue
+        } else if rightIndex >= right.count {
+            toReturn.append(left[leftIndex])
+            leftIndex += 1
+            continue
+        }
+        
+        if left[leftIndex] < right[rightIndex] {
+            toReturn.append(left[leftIndex])
+            leftIndex += 1
+        } else {
+            toReturn.append(right[rightIndex])
+            rightIndex += 1
+        }
+    }
+    
+    return toReturn
 }
