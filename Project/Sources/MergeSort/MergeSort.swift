9ccf9b4e8efcab0e17f091b47ddff40ee0ebf007
Matt Geimer <mgeimer2@illinois.edu>
2021-01-10 20:17:37 -0600

    docs: Add documentation for main functions

--- Project/Sources/MergeSort/MergeSort.swift
+++ Project/Sources/MergeSort/MergeSort.swift
@@ -1,13 +1,24 @@
 import Foundation
 
+/// Checks for any edge cases and returns recursive call to MergeSortHelper
+/// - Parameter arr: array to be sorted
+/// - Returns: sorted array
 public func MergeSort<T: Comparable>(_ arr: [T]) -> [T] {
     return arr
 }
 
+/// Recursively sorts arrays by dividing and conquering
+/// - Parameter arr: array to be sorted
+/// - Returns: sorted array
 private func MergeSortHelper<T: Comparable>(_ arr: [T]) -> [T] {
     return arr
 }
 
+/// Merges two arrays from smallest to largest
+/// - Parameters:
+///   - left: the first array to merge from
+///   - right: the second array to merge from
+/// - Returns: The combined left and right arrays
 private func Merge<T: Comparable>(_ left: [T], _ right: [T]) -> [T] {
     return left
 }
